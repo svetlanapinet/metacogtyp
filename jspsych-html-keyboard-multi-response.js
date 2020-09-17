51ec681d6a0e0524a6253e3f92e1d6e814fdf11a
Svetlana Pinet <svetlana.pinet@gmail.com>
2020-09-17 11:17:18 +0000

    Update jspsych-html-keyboard-multi-response.js

--- jspsych-html-keyboard-multi-response.js
+++ jspsych-html-keyboard-multi-response.js
@@ -129,16 +129,7 @@ jsPsych.plugins["html-keyboard-multi-response"] = (function() {
         new_html += trial.prompt;
       }
 
-    if (trial.progress_bar == true){
-      var dur = trial.trial_duration / 1000;
-      var pgbar_html = '<div style="width: 400px; height: 20px; background-color: #ccc;">' +
-    '<div style="width: 0%; height: 20px; position:top; background-color: #333; animation-name: progress-bar; animation-duration:'+dur.toString()+'s;animation-timing-function: linear;">' +
-    '</div>' +
-    '<style> @keyframes progress-bar { 0% { width:0%;} 100% { width:100%} } </style>';
-      new_html += pgbar_html;
-    }
-    console.log(pgbar_html);
-    
+   
 
 
       // draw
@@ -179,6 +170,17 @@ jsPsych.plugins["html-keyboard-multi-response"] = (function() {
 //        text_element.addEventListener('input');
 //    }
 
+    // add progress bar
+    if (trial.progress_bar == true){
+      var dur = trial.trial_duration / 1000;
+      var pgbar_html = '<div style="width: 400px; height: 20px; background-color: #ccc;">' +
+    '<div style="width: 0%; height: 20px; position:top; background-color: #333; animation-name: progress-bar; animation-duration:'+dur.toString()+'s;animation-timing-function: linear;">' +
+    '</div>' +
+    '<style> @keyframes progress-bar { 0% { width:0%;} 100% { width:100%} } </style>';
+      display_element.append(pgbar_html);
+    }
+    console.log(pgbar_html);
+ 
     // store response
 //    var response = {
 //      rt: null,
